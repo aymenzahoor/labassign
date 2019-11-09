45e4c8c8343818cacee9d27bd7a5c69a2b48fc80
aymenzahoor <aymenzahoor124@gmail.com>
2019-11-09 19:43:40 +0500

    labassign

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,51 @@
+@extends('layout')
+
+@section('title','FA17-BSE-093')
+
+@section('navbar')
+@parent
+@endsection
+
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+<link href="css/bootstrap-theme.css" rel="stylesheet";
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+<div class="container" style="border :1px solid">
+<div class="row">
+    <div class="col-md-3" style="border: 5px groove">laraib </div>
+    <div class="col-md-3" style="border: 5px groove">misbah </div>
+    <div class="col-md-3" style="border: 5px groove">aymen</div>
+    <div class="col-md-3" style="border: 5px groove">alishba </div>
+</div>
+
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12" >
+            <center><h2>name</h2></center>
+           <center><p>i am aymen i am in scripting lab</p></center>
+        </div>
+    </div>
+</div>
+<div class="container" style="border :1px solid">
+<div class="row">
+    <div class="col-md-3" style="border: 5px groove;background-color:red">laraib </div>
+    <div class="col-md-3" style="border: 5px groove;background-color:yellow">misbah </div>
+    <div class="col-md-3" style="border: 5px groove;background-color:blueviolet">aymen</div>
+    <div class="col-md-3" style="border: 5px groove;background-color:cornsilk">alishba </div>
+</div>
+
+</div>
+</body>
+</html>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+     <title>aymen zahoor - @yield('title')</title>
+  </head>
+  <body>
+
+
+
+ 
+      @section('navbar')
+     <h1>this is from section</h1>
+  @show
+
+
+
+
+  </body>
+</html>
